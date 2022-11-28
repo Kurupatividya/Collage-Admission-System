6860c15c7d05aed2311b64ff2aa0858e8d2adb25
Kurupatividya <109334243+Kurupatividya@users.noreply.github.com>
2022-11-28 21:01:30 +0530

    Update and rename README.md to sql.py

---
+++ sql.py
@@ -0,0 +1,10 @@
+# Collage-Admission-System
+import sqlite3
+
+conn=sqlite3.connect('database.db')
+
+conn.execute('CREATE TABLE STUDENTS(name varchar(255),father varchar(255),num INTEGER,email varchar(255),rank INTEGER,telephone INTEGER,dorm TEXT)')
+
+
+conn.close()
+prasanna
